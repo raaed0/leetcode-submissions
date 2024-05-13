c19fc16670f26dae0f42658542c8613341dd21ab
Masum <masum.0xr@gmail.com>
2024-05-13 10:11:41 +0000

    [LeetCode Sync] Runtime - 28 ms (12.08%), Memory - 3.6 MB (100.00%)

--- my-folder/0001-two-sum/solution.go
+++ my-folder/0001-two-sum/solution.go
@@ -1,12 +1,13 @@
 func twoSum(nums []int, target int) []int {
-    for i := 0; i < len(nums); i++ {
-		for j := 0; j < len(nums); j++ {
-			if i != j && nums[i]+nums[j] == target {
+    length := len(nums)
+    for i := 0; i < length-1; i++ {
+		for j := i+1; j < length; j++ {
+			if nums[i]+nums[j] == target {
 				slice := []int{i, j}
 				return slice
 			}
 		}
 	}
 
-	return nil
+	return []int{}
 }
