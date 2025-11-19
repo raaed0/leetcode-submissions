51954f2b6982b76045de98a6ede653e819516c89
Masum <masum.0xr@gmail.com>
2025-11-19 03:53:48 +0000

    [LeetCode Sync] Runtime - 7 ms (63.35%), Memory - 6.2 MB (50.18%)

---
+++ my-folder/0141-linked-list-cycle/solution.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+    if head == nil || head.Next == nil {
+        return false
+    }
+
+    slow := head
+    fast := head
+
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+        if fast == slow {
+            return true
+        }
+    }
+
+    return false
+}
