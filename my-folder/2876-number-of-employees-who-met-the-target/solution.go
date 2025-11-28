cabf150402f45b016dace2b9b19591655c3a9383
Masum <masum.0xr@gmail.com>
2025-11-28 17:26:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 4.5 MB (37.14%)

---
+++ my-folder/2876-number-of-employees-who-met-the-target/solution.go
@@ -0,0 +1,10 @@
+func numberOfEmployeesWhoMetTarget(hours []int, target int) int {
+    count := 0
+    for _, v := range hours {
+        if v >= target {
+            count++
+        }
+    }
+
+    return count
+}
