4e61a89332b87d19e143ff3d8f964c7b04bb5fb6
Masum <masum.0xr@gmail.com>
2025-12-11 16:14:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (61.02%)

---
+++ my-folder/0226-invert-binary-tree/solution.rs
@@ -0,0 +1,37 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(node) = root.clone() {
+            let mut n = node.borrow_mut();
+            let tmp = n.left.take();
+            n.left = n.right.take();
+            n.right = tmp;
+
+            Self::invert_tree(n.left.clone());
+            Self::invert_tree(n.right.clone());
+
+            return root;
+        }
+
+        None
+    }
+}
