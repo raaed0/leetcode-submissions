c2166de694db3a7d51da86063bc2e87d9ad52679
Masum <masum.0xr@gmail.com>
2025-12-19 16:28:47 +0000

    [LeetCode Sync] Runtime - 10 ms (51.56%), Memory - 9.2 MB (27.93%)

---
+++ my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    
+    if root == nil || p == root || q == root {
+        return root
+    }
+
+    l := lowestCommonAncestor(root.Left, p, q)
+    r := lowestCommonAncestor(root.Right, p, q)
+
+    if l == nil {
+        return r
+    } else if r == nil {
+        return l
+    } else {
+        return root
+    }
+
+}
