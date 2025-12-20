32d7dde53f949c53a8e8b1fd1d37a142c5a2253c
Masum <masum.0xr@gmail.com>
2025-12-20 16:13:25 +0000

    [LeetCode Sync] Runtime - 65 ms (45.81%), Memory - 66.6 MB (7.31%)

---
+++ my-folder/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+	if (root === null || p === root || q === root) {
+        return root
+    }
+
+    let left = lowestCommonAncestor(root.left, p, q)
+    let right = lowestCommonAncestor(root.right, p, q)
+
+    if (left === null) {
+        return right
+    } else if (right === null) {
+        return left
+    } else {
+        return root
+    }
+};
