37e28e95cb385c4a06a9f220811890e2f4991d82
Masum <masum.0xr@gmail.com>
2026-02-23 15:00:37 +0000

    [LeetCode Sync] Runtime - 4 ms (43.42%), Memory - 9.3 MB (96.90%)

---
+++ my-folder/0238-product-of-array-except-self/solution.go
@@ -0,0 +1,18 @@
+func productExceptSelf(nums []int) []int {
+    n := len(nums)
+    res := make([]int, n)
+
+    prefix := 1
+    for i := range nums {
+        res[i] = prefix
+        prefix *= nums[i]
+    }
+
+    postfix := 1
+    for i :=  n-1; i >= 0; i-- {
+        res[i] *= postfix
+        postfix *= nums[i]
+    }
+
+    return res
+}
