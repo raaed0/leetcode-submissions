666d2b894d2915d42588c6f927292e27856e1e95
Masum <masum.0xr@gmail.com>
2026-03-14 15:50:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 3.9 MB (71.43%)

---
+++ my-folder/1236-n-th-tribonacci-number/solution.go
@@ -0,0 +1,16 @@
+func tribonacci(n int) int {
+    if n <= 2 {
+        if n==0 {
+            return 0
+        }
+        return 1
+    }
+
+    dp := make([]int, n+1)
+    dp[1], dp[2] = 1, 1
+
+    for i:=3; i <= n; i++ {
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    }
+    return dp[n]
+}
