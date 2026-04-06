9a4d0fe624e04b2b8d27b5eed4b4ba643be3da82
Masum <masum.0xr@gmail.com>
2026-04-06 17:03:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9 MB (100.00%)

---
+++ my-folder/0890-lemonade-change/solution.go
@@ -0,0 +1,27 @@
+func lemonadeChange(bills []int) bool {
+    five, ten := 0, 0
+
+    for _, b := range bills {
+        if b == 5 {
+            five++
+        } else if b == 10 {
+            if five > 0 {
+                ten++
+                five--
+            } else {
+                return false
+            }
+        } else {
+            if five > 0 && ten > 0 {
+                ten--
+                five--
+            } else if five >= 3 {
+                five -= 3
+            } else {
+                return false
+            }
+        }
+    }
+
+    return true
+}
