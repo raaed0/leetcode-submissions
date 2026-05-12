6bf96c7e2509092381ad6c478bdc087ff41c06a5
Masum <masum.0xr@gmail.com>
2026-05-12 15:59:45 +0000

    [LeetCode Sync] Runtime - 240 ms (71.89%), Memory - 77.3 MB (32.43%)

---
+++ my-folder/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minReorder(self, n, connections):
+        self.res = 0    
+        roads = set()
+        graph = collections.defaultdict(list)
+        for u, v in connections:
+            roads.add((u, v))
+            graph[v].append(u)
+            graph[u].append(v)
+        def dfs(u, parent):
+            self.res += (parent, u) in roads
+            for v in graph[u]:
+                if v == parent:
+                    continue
+                dfs(v, u)    
+        dfs(0, -1)
+        return self.res
+        
