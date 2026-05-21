84a14c0806e09a804679183e2d87681a655a32f0
Masum <masum.0xr@gmail.com>
2026-05-21 16:47:46 +0000

    [LeetCode Sync] Runtime - 4 ms (31.10%), Memory - 19.5 MB (96.60%)

---
+++ my-folder/1184-car-pooling/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        lengthOfTrip = [ 0 for _ in range(1001)]
+        
+        for trip, i, j in trips:
+            lengthOfTrip[i] += trip
+            lengthOfTrip[j] -= trip
+    
+        carLoad = 0
+        for i in range( len(lengthOfTrip) ):
+            carLoad += lengthOfTrip[i] 
+            if carLoad > capacity:
+                return False                
+        return True
