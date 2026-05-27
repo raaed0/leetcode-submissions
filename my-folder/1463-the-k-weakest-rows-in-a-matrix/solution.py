0ee346f3c0aa9ff9bc5521466d1f271a15fcdcd3
Masum <masum.0xr@gmail.com>
2026-05-27 15:49:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (90.00%)

---
+++ my-folder/1463-the-k-weakest-rows-in-a-matrix/solution.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def kWeakestRows(self, mat, k):
+        row_strength = [(sum(row), i) for i, row in enumerate(mat)]
+        row_strength.sort(key=lambda x: (x[0], x[1]))
+        return [row[1] for row in row_strength[:k]]
