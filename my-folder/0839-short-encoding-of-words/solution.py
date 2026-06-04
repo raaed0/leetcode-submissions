68ab5c92456a9f2c828bee93af2963f98560020e
Masum <masum.0xr@gmail.com>
2026-06-04 15:43:42 +0000

    [LeetCode Sync] Runtime - 95 ms (20.59%), Memory - 15 MB (8.82%)

---
+++ my-folder/0839-short-encoding-of-words/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minimumLengthEncoding(self, words):
+        words = list(set(words))
+        Trie = lambda: collections.defaultdict(Trie)
+        trie = Trie()
+        nodes = [reduce(dict.__getitem__, word[::-1], trie)
+                 for word in words]
+                 
+        return sum(len(word) + 1
+                   for i, word in enumerate(words)
+                   if len(nodes[i]) == 0)
+        
