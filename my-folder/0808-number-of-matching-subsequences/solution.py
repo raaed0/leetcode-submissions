e9f0924552f31c3289f9a77515384228eb813078
Masum <masum.0xr@gmail.com>
2026-06-20 16:49:07 +0000

    [LeetCode Sync] Runtime - 286 ms (95.24%), Memory - 15.2 MB (50.00%)

---
+++ my-folder/0808-number-of-matching-subsequences/solution.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+class Solution(object):
+    def numMatchingSubseq(self, S, words):
+        word_dict = defaultdict(list)
+        count = 0
+        
+        for word in words:
+            word_dict[word[0]].append(word)            
+        
+        for char in S:
+            words_expecting_char = word_dict[char]
+            word_dict[char] = []
+            for word in words_expecting_char:
+                if len(word) == 1:
+                    count += 1
+                else:
+                    word_dict[word[1]].append(word[1:])
+        
+        return count
