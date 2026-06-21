6045e59d65823b0be98d50b959da1523ba7258d7
Masum <masum.0xr@gmail.com>
2026-06-21 18:31:29 +0000

    [LeetCode Sync] Runtime - 181 ms (67.07%), Memory - 27.1 MB (68.90%)

---
+++ my-folder/2339-k-divisible-elements-subarrays/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countDistinct(self, nums: List[int], k: int, p: int) -> int:
+        n = len(nums)                        
+        sub_arrays = set()
+        
+        for start in range(n):
+            cnt = 0
+            temp = ''
+            for i in range(start, n):
+                if nums[i]%p == 0:
+                    cnt+=1                 
+                temp+=str(nums[i]) + ','     
+                if cnt>k:
+                    break
+                sub_arrays.add(temp)                                    
+                
+        return len(sub_arrays)
